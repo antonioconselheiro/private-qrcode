cf2e3cc9c5dcaeb4a942a62977c1088d0ac1dd33
António Conselheiro <canudos.sudestino@gmail.com>
2023-11-02 09:06:45 -0300

    codificando abertura de dialog para carregamento de imagens

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import QrScanner from 'qr-scanner';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+  uploadPicture(): void {
+    const input = document.createElement('input');
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'image/*');
+    input.click();
+    input.addEventListener('change', event => {
+      debugger;
+    })
+    // QrScanner.scanImage(image);
+  }
 }
