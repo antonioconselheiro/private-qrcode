23ad4a2863545fd36e6a5e1d59f60f12f26ea996
António Conselheiro <canudos.sudestino@gmail.com>
2023-11-26 12:20:48 -0300

    codificando escrita de iv aleatório e leitura do iv contido no qrcode

--- src/app/shared/crypt/crypt.service.ts
+++ src/app/shared/crypt/crypt.service.ts
@@ -6,32 +6,52 @@ import * as CryptoJS from 'crypto-js';
 })
 export class CryptService {
 
-  private readonly initializationVector = CryptoJS.enc.Hex.parse('c2fc131ce578976a2617fd14aef014eb');
   private readonly mode = CryptoJS.mode.CBC;
   private readonly padding = CryptoJS.pad.Pkcs7;
 
   encrypt(content: string, key: string): string {
-    return `encrypted:aes?iv=${this.initializationVector};` + CryptoJS.AES.encrypt(content, String(key), {
-      iv: this.initializationVector,
+    const initializationVector = CryptoJS.enc.Hex.parse(
+      CryptoJS.lib.WordArray.random(128/8).toString()
+    );
+
+    return `encrypted:aes?iv=${initializationVector};` + CryptoJS.AES.encrypt(content, String(key), {
+      iv: initializationVector,
       mode: this.mode,
       padding: this.padding
     });
   }
 
   decrypt(encrypted: string, key: string): string {
-    /**
-     * TODO: quando for permitido customizar o iv ou houverem
-     * outros apps utilizando o mesmo formato, então será urgente
-     * ter a leitura do iv para decriptar e a leitura do modelo
-     * de criptografia selecionado
-     */
-    encrypted = encrypted.replace(/^.+;/, '');
-    const decrypted = CryptoJS.AES.decrypt(encrypted, key, {
-      iv: this.initializationVector,
-      mode: this.mode,
-      padding: this.padding
-    });
+    const parsed = this.parseEncryptedQrcode(encrypted);
+    if (parsed.encrypted && parsed.type === 'aes') {
+      const decrypted = CryptoJS.AES.decrypt(encrypted, key, {
+        iv: parsed.iv,
+        mode: this.mode,
+        padding: this.padding
+      });
+  
+      return CryptoJS.enc.Utf8.stringify(decrypted);
+    } else {
+      return parsed.content;
+    }
+  }
 
-    return CryptoJS.enc.Utf8.stringify(decrypted);
+  private parseEncryptedQrcode(encrypted: string): {
+    encrypted: true;
+    type: 'aes';
+    iv: CryptoJS.lib.WordArray;
+    content: string;
+  } | {
+    encrypted: false;
+    content: string;
+  } {
+    //  pode achar feio meu parser, mas vou melhorar ele, não precisa sair perfeito de primeira
+    if (/^encrypted:aes/.test(encrypted)) {
+      const iv = encrypted.replace(/(^.*iv=|;.*$)/g, '');
+      const content = encrypted.replace(/^.*;/, '');
+      return { encrypted: true, type: 'aes', iv: CryptoJS.enc.Hex.parse(iv), content };
+    } else {
+      return { encrypted: false, content: encrypted };
+    }
   }
 }
