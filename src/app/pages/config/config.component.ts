9ebcd129c259eda04334c408bf83a98553d7105d
António Conselheiro <canudos.sudestino@gmail.com>
2024-02-21 18:47:02 -0300

    rascunhando formulário de configuração do algoritimo

--- src/app/pages/config/config.component.ts
+++ src/app/pages/config/config.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-config',
@@ -7,4 +8,36 @@ import { Component } from '@angular/core';
 })
 export class ConfigComponent {
 
+  submitted = false;
+
+  form = this.formBuilder.group({
+    algorithm: ['aes/cbc', [
+      Validators.required.bind(this)
+    ]],
+    kdfHasher: ['sha256', [
+      Validators.required.bind(this)
+    ]],
+    kdfRounds: ['32', [
+      Validators.required.bind(this)
+    ]]
+  });
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+
+  getErrors(property: string, errorName: string): boolean {
+    const controls: any = this.form.controls;
+    return this.submitted &&
+      controls[property]?.errors &&
+      controls[property].errors[errorName] || false;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+    if (this.form.valid) {
+      const raw = this.form.getRawValue();
+
+    }
+  }
 }
--- src/app/pages/config/config.module.ts
+++ src/app/pages/config/config.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ConfigComponent } from './config.component';
 
 @NgModule({
@@ -7,7 +8,8 @@ import { ConfigComponent } from './config.component';
     ConfigComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
   exports: [
     ConfigComponent
