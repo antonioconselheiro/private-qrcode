a3360489bc1bbf852c420acad8498691af01d437
António Conselheiro <canudos.sudestino@gmail.com>
2024-03-05 17:46:02 -0300

    configurando opções de build em cordova

---
+++ build.sh
@@ -0,0 +1,10 @@
+rm -rf dist docs ./PrivateQRCode/www
+mkdir -p PrivateQRCode/www
+ng build
+cp -r ./docs/** ./PrivateQRCode/www
+cd ./PrivateQRCode
+cordova build android
+cd ..
+mkdir dist
+cp ./PrivateQRCode/platforms/android/app/build/outputs/apk/debug/app-debug.apk dist
+chmod 777 -R dist
